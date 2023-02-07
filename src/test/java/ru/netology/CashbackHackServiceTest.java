b53a622a9c70523e1e59ea07cb79449025f7ddf9
Konstantin Kharun <konstantin19980209@gmail.com>
2023-02-07 13:03:28 +0300

    newbranch

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainIfSmaller1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfSmooth1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfMore1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        Assert.assertEquals(actual, expected);
+    }
+}
